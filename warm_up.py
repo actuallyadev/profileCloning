5bcb7ba02909ffc9414bdd58861aee55b7abc911
SuperDXCEL <thedmanmanman@gmail.com>
2025-05-24 20:13:59 +0200

    The script runs perfectly although there are some functions that still need implementation to ensure correct behaviour. Implemented the act_like_a_human_function and a couple helper functions although the time check is not working as the driver is not quitting after the designated time I should also remove the porn websites or run them in headless mode, i do not know, just handle them differently

--- warm_up.py
+++ warm_up.py
@@ -20,7 +20,21 @@
 
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+import time
+import string
+import random
 from pathlib import Path
+
+TOP_WEBSITES = [
+    'google.com', 'youtube.com', 'facebook.com', 'instagram.com', 'chatgpt.com',
+    'x.com', 'whatsapp.com', 'reddit.com', 'yahoo.com', 'tiktok.com',
+    'amazon.com', 'baidu.com', 'microsoftonline.com', 'linkedin.com', 'pornhub.com',
+    'netflix.com', 'naver.com', 'live.com', 'office.com', 'bing.com',
+    'temu.com', 'pinterest.com', 'bilibili.com', 'xvideos.com', 'microsoft.com',
+    'xhamster.com', 'vk.com', 'sharepoint.com', 'fandom.com', 'globo.com',
+    'canva.com', 'weather.com', 'samsung.com', 'duckduckgo.com', 'openai.com',
+    'xnxx.com', 'nytimes.com', 'stripchat.com', 'aliexpress.com', 'roblox.com'
+]
 
 def how_many_profiles():
     """
@@ -29,7 +43,7 @@ def how_many_profiles():
     """
     try:
         return int(input("How many profiles do you want to warmup?\n"))
-    except:
+    except Exception as e:
         raise SystemExit("Not an integer, try again\n")
     
 def how_much_time():
@@ -39,7 +53,7 @@ def how_much_time():
     """
     try:
         return int(input("Specify the time in minutes that each profile should warm-up\n"))
-    except:
+    except Exception as e:
         raise SystemExit("Not an integer, try again\n")
 
 def set_up_driver(i: int):
@@ -52,32 +66,39 @@ def set_up_driver(i: int):
     chrome_options = uc.ChromeOptions()
     chrome_options.add_argument(f'--user-data-dir={user_data_dir_path}')
     chrome_options.add_argument('--profile-directory=myprofile')
-    chrome_options.add_argument('--headless=new')
     print("Just loaded: ", user_data_dir_path, "+ myprofile")
     return uc.Chrome(options=chrome_options)
 
-def get_top_websites(driver):
-    """
-        Get the top 40 websites from wikipedia
-    """
-    try:
-        driver.get('https://en.wikipedia.org/wiki/List_of_most-visited_websites')
-        top_websites_elements = driver.find_elements(By.XPATH, f'//*[contains(text(), ".com")]')
-        print("Important", top_websites_elements)
-        for website in top_websites_elements:
-            print(website.text)
-        return [website.text for website in top_websites_elements]
-    except:
-        print("Important")
-
-def get_random_websites(websites):
+def get_random_websites():
     """
         Using the random module and the choices function
         we can easily get k random websites in one line
     """
-    import random
+    return random.choices(TOP_WEBSITES, k=7)
 
-    return random.choices(websites, k=7)
+def get_random_letter():
+    """
+        Return a random letter
+    """
+    return random.choice(string.ascii_letters)
+
+def find_element(driver, random_letter):
+    """
+        Helper function to simplify act_like_a_human_logic
+    """
+    try:
+        return driver.find_element(By.XPATH, f'//*[contains(text(), "{random_letter}")]')
+    except Exception as e:
+        print("Error: ", e)
+
+def interact_with_element(element):
+    """
+        Interact with the element:
+        Click it, if it is not clickable
+        try to write something, if that fails
+        try something else
+    """
+    pass
 
 def act_like_a_human(driver, website):
     """
@@ -86,20 +107,31 @@ def act_like_a_human(driver, website):
         just enough to get proper cookies and gain
         reputation.
     """
-    pass
+    try:
+        # Without https:// it was not working
+        driver.get(f"https://{website}")
+    except Exception as e:
+        print("Importante: ", e)
+    random_scroll_value = random.choice(range(0, 1080))
+    driver.execute_script(f"window.scrollTo(0, {random_scroll_value})")
+    for i in range(0, random.choice(range(1, 15))):
+        random_letter = get_random_letter()
+        element = find_element(driver, random_letter)
+        interact_with_element(element)
+        time.sleep(1.5)
 
-def warm_up(driver, duration: int):
+def warm_up(driver, duration: int, starting_timestamp):
     """
         Warm up the environment and profile
     """
     try:
-        while duration:
-            websites = get_top_websites(driver)
-            random_websites = get_random_websites(websites)
+        # Could store in a variable but I like being a G
+        while abs(time.time() - (starting_timestamp + float(duration*60))) > 0.1:
+            random_websites = get_random_websites()
             for website in random_websites:
                 act_like_a_human(driver, website)
-    except:
-        print("Important")
+    except Exception as e:
+        print("Important: ", e)
 
 def get_data(i: int):
     """
@@ -119,13 +151,14 @@ def __main__():
         and how much time in minutes to warm-up
     """
     number_of_profiles = how_many_profiles()
-    duration_of_warm_up = how_much_time()
+    duration_of_warm_up, current_timestamp = how_much_time(), time.time()
     for i in range(1, number_of_profiles+1):
         data_before = get_data(i)
         driver = set_up_driver(i)
-        warm_up(driver, duration_of_warm_up)
+        warm_up(driver, duration_of_warm_up, current_timestamp)
         data_after = get_data(i)
         check_difference(data_before, data_after)
+        driver.quit()
 
 if __name__ == '__main__':
     __main__()
